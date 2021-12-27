763aee7dcddfd5ceb0a0b556a8cc532fe6dca8bb
siykt <cnsiykt@163.com>
2021-12-27 17:08:23 +0800

    feat: 调整安装脚本

--- install.sh
+++ install.sh
@@ -1,18 +1,36 @@
-defaultInstallNodeVersion="14.3"
+# v14.3兼容性
+DEFAULT_INSTALL_NODE_VERSION="14.3"
 
-useNvmInstallNode() {
-  nvm install ${defaultInstallNodeVersion}
-  nvn use ${defaultInstallNodeVersion}
+use_nvm_install_node() {
+  nvm install ${DEFAULT_INSTALL_NODE_VERSION}
+  nvm use ${DEFAULT_INSTALL_NODE_VERSION}
 }
 
-if [[ $(uname) == 'Darwin' ]]; then
-  echo "Mac OS"
+program_not_exists() {
+  local ret='0'
+  command -v $1 >/dev/null 2>&1 || { local ret='1'; }
+  # fail on non-zero return value
+  if [ "$ret" -ne 0 ]; then
+    return 1
+  fi
+
+  return 0
+}
+
+if program_not_exists git; then
+  yum install git -y
 fi
 
-if [[ $(uname) == 'Linux' ]]; then
-  # CentOS install
-  git clone git://github.com/cnpm/nvm.git ~/.nvm && cd ~/.nvm && git checkout $(git describe --abbrev=0 --tags)
-  echo ". ~/.nvm/nvm.sh" >>/etc/profile
-  source /etc/profile
-  useNvmInstallNode
+git clone git://github.com/cnpm/nvm.git ~/.nvm && cd ~/.nvm && git checkout $(git describe --abbrev=0 --tags)
+echo ". ~/.nvm/nvm.sh" >>/etc/profile
+source /etc/profile
+use_nvm_install_node
+
+if program_not_exists nginx; then
+  yum -y install nginx
+  systemctl enable nginx
+  service nginx start
 fi
+
+# 安装 pm2
+npm install pm2 -g
