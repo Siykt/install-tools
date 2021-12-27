cb44042af984a2d8ec050e297b5134fe73824281
siykt <cnsiykt@163.com>
2021-12-27 16:29:23 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,18 @@
+defaultInstallNodeVersion="14.3"
+
+installNvm() {
+
+  nvm install ${defaultInstallNodeVersion}
+  nvn use ${defaultInstallNodeVersion}
+}
+
+if [[ $(uname) == 'Darwin' ]]; then
+  echo "Mac OS"
+fi
+
+if [[ $(uname) == 'Linux' ]]; then
+  # CentOS install
+  git clone git://github.com/cnpm/nvm.git ~/.nvm && cd ~/.nvm && git checkout $(git describe --abbrev=0 --tags)
+  echo ". ~/.nvm/nvm.sh" >>/etc/profile
+  source /etc/profile
+fi
